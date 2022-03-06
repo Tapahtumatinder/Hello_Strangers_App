2a3d334d3e9a1e506d35ade925fc396c65ce656a
Tiia Haverinen <bgo844@myy.haaga-helia.fi>
2022-03-06 21:45:23 +0200

    save birthdate

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -5,24 +5,32 @@ import { auth, db } from '../firebase';
 import { React, useState, useEffect } from 'react';
 import DatePicker from 'react-native-date-picker'
 import DateTimePicker from '@react-native-community/datetimepicker';
-import styles from '../AppStyle'
+import styles from '../AppStyle';
+import Picture from '../components/Picture';
+
 
 const ProfileScreen = () => {
     const [userName, setUserName] = useState('');
     const [userDescription, setUserDescription]= useState('');
     const [userAge, setUserAge]= useState('');
-    
+    const [bDayText, setbDayText] = useState('Empty');
+    const [userBirthdate, setUserBirthdate]= useState(new Date());
+   
     //Pick the birthdate
     const [date, setDate]= useState(new Date());
     const [show, setShow] = useState(false);
     const [chosenDate, setChosenDate] = useState();
     
-    // calculate age
+    // select date
     const onDateChange = (event, selectedDate) => {
       const currentDate = selectedDate || date;
       setShow(Platform.OS === 'ios');
       setDate(currentDate);
       setChosenDate(selectedDate);
+      var tempDate= new Date(currentDate);
+      var fDate= tempDate.getDate() + '/' + (tempDate.getMonth()+ 1) +'/' + tempDate.getFullYear();
+      setbDayText(fDate);
+    // calculate age
       var birthDate= new Date(selectedDate);
       var currentDay= new Date();
       var age= currentDay.getFullYear()-birthDate.getFullYear();
@@ -30,7 +38,9 @@ const ProfileScreen = () => {
       if(month<0||(month===0 && currentDay.getDate()<birthDate.getDate()))
       age --;
       setUserAge(age);
+      setUserBirthdate(birthDate);
       console.log(age);
+      console.log(birthDate);
       setShow(false);
     };
 
@@ -49,6 +59,7 @@ const ProfileScreen = () => {
         setUserName(docSnap.data().userName);
         setUserDescription(docSnap.data().userDescription);
         setUserAge(docSnap.data().userAge);
+        setUserBirthdate(docSnap.data().userBirthdate);
       } else {
         console.log('a true stranger');
       }
@@ -61,12 +72,14 @@ const ProfileScreen = () => {
       await setDoc(doc(db, 'user', auth.currentUser.uid), {
         userName: userName,
         userDescription: userDescription,
-        userAge : userAge
+        userAge : userAge,
+        userBirthdate: userBirthdate
     })
   }
     return (
       <View style= {styles.mainContainer}>
         <View  style= {styles.inputContainer}>
+          
         <Button buttonStyle= {styles.basicButton} title ='ADD PHOTO' titleStyle={styles.basicTitle}/>
         <Text>Name</Text>
         <TextInput
@@ -86,13 +99,14 @@ const ProfileScreen = () => {
             />
            
           <Button buttonStyle= {styles.basicButton} title ="SELECT BIRTHDAY" titleStyle={styles.basicTitle} onPress={() => setShow(true)} />
+         
+         <Text>{bDayText}</Text>
           </View>
             {show && (
             <DateTimePicker
               testID="dateTimePicker"
-              value={date}
+              value= {date}
               mode="date"
-              is24Hour={true}
               display="default"
               onChange={onDateChange}
             />
