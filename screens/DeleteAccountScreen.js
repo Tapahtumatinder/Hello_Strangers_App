2839880caed1387a57de332a58125aa36cd25684
Tiia Haverinen <bgo844@myy.haaga-helia.fi>
2022-04-05 08:31:22 +0300

    delete profile

--- screens/DeleteAccountScreen.js
+++ screens/DeleteAccountScreen.js
@@ -6,52 +6,56 @@ import { getAuth, reauthenticateWithCredential, EmailAuthProvider, deleteUser } 
 import { auth, db } from '../firebase'
 import { Button } from "react-native-elements";
 import { deleteDoc, doc } from "@firebase/firestore";
+import { collection } from "@firebase/firestore";
+import { Firestore } from "@firebase/firestore";
+import { DocumentReference } from "@firebase/firestore";
+import { getFirestore } from 'firebase/firestore'
 
 
 const DeleteAccountScreen=({navigation})=>{
-
+const db= getFirestore();
+const colRef= collection(db,'user');
 const [password, setPassword]= useState(''); 
-const auth = getAuth()
+const auth = getAuth();
+const docRef= doc(db,'user', auth.currentUser.uid)
 const user= auth.currentUser;
-const credential = EmailAuthProvider.credential(
-    auth.currentUser.email,
-    password
-)
+
+
+const credential = EmailAuthProvider.credential(auth.currentUser.email,password);
 const result = async () => {
-    await reauthenticateWithCredential(auth.currentUser,credential)
-    deleteData()
+    await reauthenticateWithCredential(auth.currentUser,credential);
+    deleteDoc(docRef)
+    .then(()=>{
+        console.log('wörkkii')
     deleteUser(user).then(()=> {
         console.log("deleted")
-    }).catch((error) => {
-        console.log("joku meni pieleen")
-    })
+        //todo: redirect to login/registerscreen
+        
+    }).catch(error => alert(error.message))
 
-    .catch(error => alert(error.message))
+}).catch(error => alert(error.message))
 }
 // User successfully reauthenticated. New ID tokens should be valid.
-const deleteData = async () => {
-    const docRef = doc(db, 'user', auth.currentUser.uid);
-    const docSnap = await getDoc(docRef);
 
-    if (docSnap.exists()) {
-    await  deleteDoc(doc(db,"user", auth.currentUser.uid));
-    } else {
-      console.log('a true stranger');
-    }
-  }
+
+
 
 
     return(
         <View>
-<Text>If you are sure you want to delete your account, give your password</Text>
-        <TextInput placeholder='Password'
+            <Text style={{marginTop:40, textAlign:'center'}}>To delete your account, give your password</Text>
+                <View style= {{alignItems:'center', paddingTop:20, paddingBottom:20}}><TextInput 
+                    placeholder='Password'
                     value={password}
                     onChangeText={text => setPassword(text)}
                     style= {styles.input}
                     secureTextEntry
                     />
-                <Button onPress= {result} title='Delete'/>
-</View>
+                    
+                <Button onPress= {result} title='Delete' style={{width: 100, paddingTop:20}}/>
+                </View>
+        </View>
     )
 }
+
 export default DeleteAccountScreen;
